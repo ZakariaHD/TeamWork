f6dcd4c4979f78848cb0adbe6becc48b0d41ae14
ziaritarik <ziaritarik.u@gmail.com>
2020-04-19 18:06:38 +0100

    fix erreur scripte de la création du la base de donnée TeamWork

--- TeamWork.sql
+++ TeamWork.sql
@@ -1,57 +1,51 @@
-USE [master]
+USE master
 GO
-CREATE LOGIN uwork WITH PASSWORD='uwork' MUST_CHANGE,
-DEFAULT_DATABASE=[master], 
-CHECK_EXPIRATION=ON,
-CHECK_POLICY=ON
-GO
-EXEC master..sp_addsrvrolemember @loginame = 'uwork', @rolename = 'sysadmin'
-GO
-USE [master]
-GO
-CREATE USER uwork FOR LOGIN uwork
-GO
-USE [model]
-GO
-CREATE USER uwork FOR LOGIN uwork
-GO
-USE [msdb]
-GO
-CREATE USER uwork FOR LOGIN uwork
-GO
-USE [tempdb]
-GO
-CREATE USER uwork FOR LOGIN uwork
-GO
-USE [tempdb]
-GO
-EXEC sp_addrolemember 'db_owner', 'uwork'
-GO
-use master
+--- Variable
+---- Declaration 
+declare  @loginName VARCHAR(20)
+---- Replissage
+set  @loginName= 'uwork'
+If  not Exists (select loginname from master.dbo.syslogins   where name = @loginName)
+	
+Begin
+   CREATE LOGIN uwork WITH PASSWORD='uwork'
+End
+
+---- Declaration 
+declare  @Name VARCHAR(20)
+---- Replissage
+set  @Name= 'TeamWork'
+If  not Exists (select name from master.dbo.sysdatabases   where name=@Name)
+	
+Begin
+EXEC master.sys.xp_create_subdir 'C:\Team'
+
 CREATE DATABASE TeamWork
 ON PRIMARY
 (NAME='TeamWork',
-FILENAME='C:\TeamWork.mdf',
+FILENAME='C:\Team\TeamWork.mdf',
 SIZE=100MB,
 MAXSIZE=200MB,
 FILEGROWTH=10MB
 )
 LOG ON
 (NAME='TeamWork_log',
-FILENAME='C:\TeamWork_log.ldf',
+FILENAME='C:\Team\TeamWork_log.ldf',
 SIZE=10MB,
 MAXSIZE=200MB,
 FILEGROWTH=10%
 )
 
 
---Modification d'un fichier
+--Modification de la base données
 ----Ajout d'un ficher secondaire
 ALTER DATABASE TeamWork
 ADD FILE
 (NAME=TeamWork_data,
-FILENAME='C:\TeamWork_data.ndf',
+FILENAME='C:\Team\TeamWork_data.ndf',
 SIZE=8MB,
 MAXSIZE=100MB,
 FILEGROWTH=10MB
 );
+End
+
